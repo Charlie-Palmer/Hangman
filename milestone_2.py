a1246c146f55a700651d1d52cec0143cf80587a8
Charlie Palmer <charlesezra96@gmail.com>
2024-02-28 16:55:11 +0000

    Current stage of code

---
+++ milestone_2.py
@@ -0,0 +1,14 @@
+import random
+
+word_list = ['Mango', 'Kiwi', 'Cherry', 'Plum', 'Lychee']
+print(word_list)
+
+word = random.choice(word_list)
+
+print(word)
+Guess = input("Please enter a letter: ")
+if len(Guess) == 1 and Guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+        
